d003cfc58bc98cf1dbb2b68367a16933f51b01a6
Oleksandr Velychko <49977771+VelychkoOleksandr@users.noreply.github.com>
2020-02-22 14:03:29 +0200

    DB access link generator

---
+++ meta-data/db-access.js
@@ -0,0 +1,6 @@
+const username = 'admin'
+const password = 'Lfhk7zNdfGBr8vaC'
+
+const collection = 'cookbook'
+
+module.exports = `mongodb+srv://${username}:${password}@cluster0-3mvz8.mongodb.net/${collection}?retryWrites=true&w=majority`
